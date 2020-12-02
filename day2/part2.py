5224a4dd853c05bc1ae3dbaabffe63df02d14f70
Benjamin Segall <bentekkie@gmail.com>
2020-12-02 10:48:49 -0500

    Create part2.py

---
+++ day2/part2.py
@@ -0,0 +1,4 @@
+import re
+p=re.compile('([^-]*)-([^-]*) (.): (.*)')
+r=[p.match(l).groups() for l in open('i','r').readlines()]
+sum(1for x in r if(x[3][int(x[0])-1]==x[2])+(x[3][int(x[1])-1]==x[2])==1)
