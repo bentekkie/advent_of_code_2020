7c43dd166dfaa1b4c7a9f38d200c68bd202ca16c
Benjamin Segall <bentekkie@gmail.com>
2020-12-02 11:08:43 -0500

    Update part1.py

--- day2/part1.py
+++ day2/part1.py
@@ -1,2 +1,2 @@
 import re
-print(sum(1for x in[re.compile('([^-]*)-([^-]*) (.): (.*)').match(l).groups() for l in open('i','r')]if int(x[0])<=x[3].count(x[2])<=int(x[1])))
+print(sum(1for x in[re.compile('([^-]*)-([^-]*) (.): (.*)').match(l).groups()for l in open('i','r')]if int(x[0])<=x[3].count(x[2])<=int(x[1])))
