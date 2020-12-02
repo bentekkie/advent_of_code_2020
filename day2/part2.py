b4f213f37bc515204079c33bfbea166f63c60962
Benjamin Segall <bentekkie@gmail.com>
2020-12-02 10:49:46 -0500

    Update part2.py

--- day2/part2.py
+++ day2/part2.py
@@ -1,4 +1,3 @@
 import re
-p=re.compile('([^-]*)-([^-]*) (.): (.*)')
-r=[p.match(l).groups() for l in open('i','r').readlines()]
+r=[re.compile('([^-]*)-([^-]*) (.): (.*)').match(l).groups() for l in open('i','r').readlines()]
 print(sum(1for x in r if(x[3][int(x[0])-1]==x[2])+(x[3][int(x[1])-1]==x[2])==1))
